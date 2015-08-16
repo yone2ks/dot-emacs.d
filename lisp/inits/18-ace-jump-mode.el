9c1724e410cef954f90d4a1ff68975cfc2dd3d24
Tsukasa Yoneda <t.yoneda.pub@gmail.com>
2015-08-16 23:18:36 +0900

    modified 18-ace-jump-mode.el for change binding

--- lisp/inits/18-ace-jump-mode.el
+++ lisp/inits/18-ace-jump-mode.el
@@ -2,6 +2,6 @@
       (append "asdfghjkl;:]qwertyuiop@zxcvbnm,." nil))
 ;; ace-jump-word-modeのとき文字を尋ねないようにする
 (setq ace-jump-word-mode-use-query-char nil)
-(bind-key* "C-c SPC" 'ace-jump-char-mode)
+(bind-key* "C-/" 'ace-jump-char-mode)
 ;;(bind-key* "C-;" 'ace-jump-word-mode)
 (bind-key* "C-M-;" 'ace-jump-line-mode)
