931bdb66371e53a64d8c59299fbf84d2135b1cad
Tsukasa Yoneda <t.yoneda.pub@gmail.com>
2015-09-07 23:46:43 +0900

    modified 50-helm.el for helm-migemo

--- lisp/inits/50-helm.el
+++ lisp/inits/50-helm.el
@@ -2,6 +2,21 @@
 (require 'cl)
 (require 'helm-config)
 (require 'helm-migemo)
+(with-eval-after-load "helm-migemo"
+  (defun helm-compile-source--candidates-in-buffer (source)
+    (helm-aif (assoc 'candidates-in-buffer source)
+        (append source
+                `((candidates
+                   . ,(or (cdr it)
+                          (lambda ()
+                            ;; Do not use `source' because other plugins
+                            ;; (such as helm-migemo) may change it
+                            (helm-candidates-in-buffer (helm-get-current-source)))))
+                  (volatile) (match identity)))
+      source))
+  ;; [2015-09-06 Sun]helm-match-plugin -> helm-multi-match変更の煽りを受けて
+  (defalias 'helm-mp-3-get-patterns 'helm-mm-3-get-patterns)
+  (defalias 'helm-mp-3-search-base 'helm-mm-3-search-base))
 (require 'helm-files)
 (require 'helm-ag)
 ; agのデフォルトのコマンドオプションを指定
